5375185cefbea7325b80642c7d9ff0f62dae0e04
rpsobolewski <rpsobolewski@gmail.com>
2023-08-29 09:43:20 +0200

    fixed mail issue

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -13,14 +13,15 @@ document.querySelector('button').addEventListener('click', function (e) {
     const mail = document.getElementById("mail").value;
     console.log(mail);
     const risultato = document.getElementById("risultato");
+    
         
 
 
     for (let i = 0; i < mails.length; i++) {
-        if (mail == mails[i]){
-            console.log("ao")
+        if (mail === mails[i]){
+            console.log("ao");
         risultato.innerHTML = "sei in lista";
-
+            break;
         } else {
         risultato.innerHTML = "non sei in lista";
 
